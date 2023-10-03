b3790b8c10ddb771c5b51cb8591e2f2575ccb0b2
Tenzin <tenzintsunduebhattu@gmail.com>
2023-10-03 18:22:12 +0530

    FEAT_function_download_file_with_url

--- src/word_aligner/config.py
+++ src/word_aligner/config.py
@@ -4,5 +4,6 @@ CURRENT_DIR = Path(__file__).resolve().parent
 PARENT_DIR = CURRENT_DIR.parent
 TOP_LEVEL_DIR = PARENT_DIR.parent
 DATA_FOLDER_DIR = TOP_LEVEL_DIR / "data"
+SUB_INPUT_1 = DATA_FOLDER_DIR / "input" / "sub_input_1"
 RESOURCE_FOLDER_DIR = CURRENT_DIR / "resources"
 LOG_FOLDER_DIR = CURRENT_DIR / "logs"
--- src/word_aligner/github_file_downloader.py
+++ src/word_aligner/github_file_downloader.py
@@ -1,9 +1,16 @@
-from github import Github
+from pathlib import Path
 
+import requests
+from github import Github
+from retrying import retry
+
+from .config import LOG_FOLDER_DIR, SUB_INPUT_1
 from .github_token import GITHUB_TOKEN
 
 TOKEN = GITHUB_TOKEN
 REPO_OWNER = "MonlamAI"
+
+ERROR_LOG_FILE = "Failed_to_download_BOs.txt"
 
 
 class GitHubFileDownloader:
@@ -46,10 +53,42 @@ class GitHubFileDownloader:
             return None
 
 
+@retry(
+    stop_max_attempt_number=3,  # Maximum number of retries
+    wait_fixed=2000,  # Delay between retries in milliseconds (2 seconds)
+    retry_on_exception=lambda x: isinstance(x, requests.exceptions.RequestException),
+)
+def download_file_with_url(
+    download_url, new_downloaded_file_name, destination_folder="."
+):
+
+    if download_url is None:
+        print("Failed to download file. Download URL is None")
+        write_to_error_log(ERROR_LOG_FILE, new_downloaded_file_name)
+        return
+    response = requests.get(download_url)
+
+    new_downloaded_file_name = new_downloaded_file_name
+    output_path = Path(destination_folder) / new_downloaded_file_name
+    if response.status_code == 200:
+        with open(output_path, "wb") as local_file:
+            local_file.write(response.content)
+        print(f"File downloaded and saved to {output_path}")
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
+        write_to_error_log(ERROR_LOG_FILE, new_downloaded_file_name)
+
+
+def write_to_error_log(error_log_file, filename):
+    error_log_file_path = LOG_FOLDER_DIR / error_log_file
+    with open(error_log_file_path, "a") as log_file:
+        log_file.write(f"{filename}: Failed to download: \n")
+
+
 if __name__ == "__main__":
     # Usage example
-    tm_file_name = "TM079"
+    tm_file_name = "TM0791"
     downloader = GitHubFileDownloader(TOKEN, REPO_OWNER, tm_file_name)
     bo_url, en_url = downloader.get_download_urls_from_repo()
-    print(bo_url)
-    print(en_url)
+    download_file_with_url(bo_url, f"{tm_file_name}-bo.txt", SUB_INPUT_1)
+    download_file_with_url(en_url, f"{tm_file_name}-en.txt", SUB_INPUT_1)
