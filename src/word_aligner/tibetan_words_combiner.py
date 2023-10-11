eb31362a1cef3262d892771e11417adc31710ad5
Tenzin <tenzintsunduebhattu@gmail.com>
2023-10-11 16:04:30 +0530

    FEAT_function_load_mahavyutapatti_word_list_and_UPDATE_mahavyutpatti_dataset

--- src/word_aligner/tibetan_words_combiner.py
+++ src/word_aligner/tibetan_words_combiner.py
@@ -7,17 +7,41 @@ from word_aligner.config import RESOURCE_FOLDER_DIR
 from word_aligner.data_processor import normalise_tsek
 
 
-def load_MONLAM_2020_word_dict():
+def load_MONLAM_2020_word_list():
     MONLAM_2020 = Path(RESOURCE_FOLDER_DIR / "སྨོན་2020-headwords.csv").read_text(
         encoding="utf-8"
     )
     MONLAM_2020_lines = normalise_tsek(MONLAM_2020).splitlines()
-    MONLAM_2020_normalised = [
+    MONLAM_2020_word_list = [
         word + TSEK if word[-1] not in ["་", "ཿ"] else word
         for word in MONLAM_2020_lines
     ]
-    MONLAM_2020_word_dict = group_words_by_first_character(MONLAM_2020_normalised)
-    return MONLAM_2020_word_dict
+    return MONLAM_2020_word_list
+
+
+def load_mahavyutpatti_word_list():
+    mahavyutpatti = Path(RESOURCE_FOLDER_DIR / "mahavyutpatti.csv").read_text(
+        encoding="utf-8"
+    )
+    mahavyutpatti_lines = normalise_tsek(mahavyutpatti).splitlines()
+    mahavyutpatti_lines = [line.split(",")[0].strip() for line in mahavyutpatti_lines]
+    mahavyutpatti_word_list = [
+        word + TSEK if word[-1] not in ["་", "ཿ"] else word
+        for word in mahavyutpatti_lines
+    ]
+    return mahavyutpatti_word_list
+
+
+def load_all_word_list():
+    word_list = load_MONLAM_2020_word_list() + load_mahavyutpatti_word_list()
+    word_list = list(set(word_list))
+    return word_list
+
+
+def load_tibetan_word_dictionary():
+    word_list = load_all_word_list()
+    word_dict = group_words_by_first_character(word_list)
+    return word_dict
 
 
 def group_words_by_first_character(word_list: List[str]) -> dict:
@@ -36,7 +60,7 @@ def group_words_by_first_character(word_list: List[str]) -> dict:
     return word_dict
 
 
-def combine_compound_words_MONLAM2020(word_dict: Dict, sentence: str):
+def merge_tibetan_compound_words(word_dict: Dict, sentence: str):
     # Input:> sentence = "ས་ ཆེན་ ཀུན་དགའ་ བློ་གྲོས་ ཡིན་པ ས་"
     # output:> sentence = "ས་+ཆེན་+ཀུན་དགའ་+བློ་གྲོས་ ཡིན་པ ས་"
     words = sentence.split()
@@ -63,7 +87,4 @@ def combine_compound_words_MONLAM2020(word_dict: Dict, sentence: str):
 
 if __name__ == "__main__":
     # Example usage:
-    text = "ང་ ས་ ཆེན་ ཀུན་དགའ་ བློ་གྲོས་ ཡིན་"
-    text = "དྷ་ ར"
-    MONLAM_2020 = load_MONLAM_2020_word_dict()
-    print(combine_compound_words_MONLAM2020(MONLAM_2020, text))
+    pass
