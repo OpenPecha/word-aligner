3a5f895e2a6bf1a545fc5025aaa7292ba794c8a3
Tenzin <tenzintsunduebhattu@gmail.com>
2023-10-13 16:02:54 +0530

    FEAT_function_write_dictionary_to_json_and_UPDATE_tibetan_english_dictionary_dict.json

--- src/word_aligner/alignment_accuracy_evaluator.py
+++ src/word_aligner/alignment_accuracy_evaluator.py
@@ -1,3 +1,4 @@
+import json
 import re
 from pathlib import Path
 
@@ -21,14 +22,18 @@ def read_excel_tibetan_to_english(file_path):
         english_words = extract_text_in_double_quotes(word_description)
         if english_words:
             data[tibetan_word] = english_words
-
     return data
+
+
+def write_dictionary_to_json(dictionary, file_path):
+    with open(file_path, "w", encoding="utf-8") as fp:
+        json.dump(dictionary, fp, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
     # Example usage:
-    CURRENT_DIR = Path(__file__).parent
-    file_path = CURRENT_DIR / "resources/Illuminator.xlsx"
+    RESOURCES_FOLDER_DIR = Path(__file__).parent / "resources"
+    file_path = RESOURCES_FOLDER_DIR / "Illuminator.xlsx"
     tibetan_english_dict = read_excel_tibetan_to_english(file_path)
-    for key, value in tibetan_english_dict.items():
-        print(f"Tibetan: {key}, English: {value}")
+    json_output_file_path = RESOURCES_FOLDER_DIR / "tibetan_english_dict.json"
+    write_dictionary_to_json(tibetan_english_dict, json_output_file_path)
