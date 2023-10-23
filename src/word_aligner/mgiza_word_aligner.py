5bdbdefafefcbf14601bf22cf9022cbbf2031d42
Tenzin <tenzintsunduebhattu@gmail.com>
2023-10-23 11:06:34 +0530

    MODIFY_tokenize_file_wise_using_antx_than_line_by_line

--- src/word_aligner/mgiza_word_aligner.py
+++ src/word_aligner/mgiza_word_aligner.py
@@ -7,6 +7,7 @@ from typing import Dict
 
 from botok import TSEK
 
+from word_aligner.annotation_transfer import newline_annotations_transfer
 from word_aligner.data_processor import (
     clean_english_text,
     clean_tibetan_text,
@@ -35,7 +36,8 @@ def tokenize_and_merge_files(
     num_files_to_train=1,
 ):
     # Paths
-    data_dir = "data"
+    base_dir = os.getcwd()
+    data_dir = os.path.join(base_dir, "data")
     input_dir = os.path.join(data_dir, "input")
     english_out_file = os.path.join(data_dir, "english.txt")
     tibetan_out_file = os.path.join(data_dir, "tibetan.txt")
@@ -56,8 +58,8 @@ def tokenize_and_merge_files(
                 english_files = [f for f in files_in_subdir if f.endswith("-en.txt")]
                 tibetan_files = [f for f in files_in_subdir if f.endswith("-bo.txt")]
 
-                english_files = sorted(english_files)[:num_files_to_train]
-                tibetan_files = sorted(tibetan_files)[:num_files_to_train]
+                english_files = sorted(english_files)
+                tibetan_files = sorted(tibetan_files)
 
                 if len(english_files) != len(tibetan_files):
                     print(
@@ -65,6 +67,7 @@ def tokenize_and_merge_files(
                     )
                     continue
 
+                files_counter = 1
                 for english_file, tibetan_file in zip(
                     sorted(english_files), sorted(tibetan_files)
                 ):
@@ -73,37 +76,50 @@ def tokenize_and_merge_files(
                     ) as eng, open(
                         os.path.join(full_subdir_path, tibetan_file), encoding="utf-8"
                     ) as bo:
-                        en_lines = eng.readlines()
-                        bo_lines = bo.readlines()
+                        # if the len are same
 
-                        for en_line, bo_line in zip(en_lines, bo_lines):
-                            if combine_english_compound_words:
-                                en_line = tokenize_english_with_named_entities(
-                                    spacy_tokenizer_obj,
-                                    clean_english_text(en_line),
-                                    english_lemma,
-                                )
-                            else:
-                                en_line = tokenize_english_with_spacy(
-                                    spacy_tokenizer_obj,
-                                    clean_english_text(en_line),
-                                    english_lemma,
-                                )
-                            bo_line = tokenize_tibetan_with_botok(
-                                botok_tokenizer_obj,
-                                clean_tibetan_text(bo_line),
-                                split_affix,
-                                tibetan_lemma,
+                        eng_content = eng.read()
+                        bo_content = bo.read()
+                        if eng_content.count("\n") != bo_content.count("\n"):
+                            continue
+                        print(
+                            f"File: [{files_counter}/{num_files_to_train}] processing ..."
+                        )
+                        if combine_english_compound_words:
+                            eng_tokenized = tokenize_english_with_named_entities(
+                                spacy_tokenizer_obj,
+                                clean_english_text(eng_content),
+                                english_lemma,
                             )
-                            if combine_tibetan_compound_words:
-                                bo_line = merge_tibetan_compound_words(
-                                    TIBETAN_WORD_DICTIONARY, bo_line
-                                )
-
-                            if en_line and bo_line:
-                                english_out.write(en_line + "\n")
-                                tibetan_out.write(bo_line + "\n")
-
+                        else:
+                            eng_tokenized = tokenize_english_with_spacy(
+                                spacy_tokenizer_obj,
+                                clean_english_text(eng_content),
+                                english_lemma,
+                            )
+                        bo_tokenized = tokenize_tibetan_with_botok(
+                            botok_tokenizer_obj,
+                            clean_tibetan_text(bo_content),
+                            split_affix,
+                            tibetan_lemma,
+                        )
+                        if combine_tibetan_compound_words:
+                            bo_tokenized = merge_tibetan_compound_words(
+                                TIBETAN_WORD_DICTIONARY, bo_tokenized
+                            )
+                        print("Tokenization done!..")
+                        eng_tokenized = newline_annotations_transfer(
+                            eng_content, eng_tokenized
+                        )
+                        bo_tokenized = newline_annotations_transfer(
+                            bo_content, bo_tokenized
+                        )
+                        if eng_tokenized and bo_tokenized:
+                            english_out.write(eng_tokenized + "\n")
+                            tibetan_out.write(bo_tokenized + "\n")
+                            files_counter += 1
+                            if files_counter >= num_files_to_train:
+                                break
     print(f"Data merged into {english_out_file} and {tibetan_out_file}.")
 
 
